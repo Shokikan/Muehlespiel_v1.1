97e8caabafdc7377e686b55ff491df38efbdb3d3
Shokikan <mark.mathis240@hotmail.ch>
2021-03-25 10:33:44 +0100

    überarbeitung des Spiels

---
+++ Muehlespiel/src/FXMLController.java
@@ -0,0 +1,240 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
+public class FXMLController implements Initializable {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Spielregeln regel;
+
+    @FXML
+    private Pane box1;
+
+    @FXML
+    private Circle circ1;
+
+    @FXML
+    private Pane box2;
+
+    @FXML
+    private Circle circ2;
+
+    @FXML
+    private Pane box3;
+
+    @FXML
+    private Circle circ3;
+
+    @FXML
+    private Pane box4;
+
+    @FXML
+    private Circle circ4;
+
+    @FXML
+    private Pane box5;
+
+    @FXML
+    private Circle circ5;
+
+    @FXML
+    private Pane box6;
+
+    @FXML
+    private Circle circ6;
+
+    @FXML
+    private Pane box7;
+
+    @FXML
+    private Circle circ7;
+
+    @FXML
+    private Pane box8;
+
+    @FXML
+    private Circle circ8;
+
+    @FXML
+    private Pane box9;
+
+    @FXML
+    private Circle circ9;
+
+    @FXML
+    private Pane box10;
+
+    @FXML
+    private Circle circ10;
+
+    @FXML
+    private Pane box11;
+
+    @FXML
+    private Circle circ11;
+
+    @FXML
+    private Pane box12;
+
+    @FXML
+    private Circle circ12;
+
+    @FXML
+    private Pane box13;
+
+    @FXML
+    private Circle circ13;
+
+    @FXML
+    private Pane box14;
+
+    @FXML
+    private Circle circ14;
+
+    @FXML
+    private Pane box15;
+
+    @FXML
+    private Circle circ15;
+
+    @FXML
+    private Pane box16;
+
+    @FXML
+    private Circle circ16;
+
+    @FXML
+    private Pane box17;
+
+    @FXML
+    private Circle circ17;
+
+    @FXML
+    private Pane box18;
+
+    @FXML
+    private Circle circ18;
+
+    @FXML
+    private Pane box19;
+
+    @FXML
+    private Circle circ19;
+
+    @FXML
+    private Pane box20;
+
+    @FXML
+    private Circle circ20;
+
+    @FXML
+    private Pane box21;
+
+    @FXML
+    private Circle circ21;
+
+    @FXML
+    private Pane box22;
+
+    @FXML
+    private Circle circ22;
+
+    @FXML
+    private Pane box23;
+
+    @FXML
+    private Circle circ23;
+
+    @FXML
+    private Pane box24;
+
+    @FXML
+    private Circle circ24;
+
+    @FXML
+    void manageMouseClick(MouseEvent event) {
+        
+    }
+
+    @FXML
+    void manageMouseClickbox2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mouseClick(MouseEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        regel = new Spielregeln();
+    }
+
+    public void 
+
+    @FXML
+    void initialize() {
+        assert box1 != null : "fx:id=\"box1\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ1 != null : "fx:id=\"circ1\" was not injected: check your FXML file 'view.fxml'.";
+        assert box2 != null : "fx:id=\"box2\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ2 != null : "fx:id=\"circ2\" was not injected: check your FXML file 'view.fxml'.";
+        assert box3 != null : "fx:id=\"box3\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ3 != null : "fx:id=\"circ3\" was not injected: check your FXML file 'view.fxml'.";
+        assert box4 != null : "fx:id=\"box4\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ4 != null : "fx:id=\"circ4\" was not injected: check your FXML file 'view.fxml'.";
+        assert box5 != null : "fx:id=\"box5\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ5 != null : "fx:id=\"circ5\" was not injected: check your FXML file 'view.fxml'.";
+        assert box6 != null : "fx:id=\"box6\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ6 != null : "fx:id=\"circ6\" was not injected: check your FXML file 'view.fxml'.";
+        assert box7 != null : "fx:id=\"box7\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ7 != null : "fx:id=\"circ7\" was not injected: check your FXML file 'view.fxml'.";
+        assert box8 != null : "fx:id=\"box8\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ8 != null : "fx:id=\"circ8\" was not injected: check your FXML file 'view.fxml'.";
+        assert box9 != null : "fx:id=\"box9\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ9 != null : "fx:id=\"circ9\" was not injected: check your FXML file 'view.fxml'.";
+        assert box10 != null : "fx:id=\"box10\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ10 != null : "fx:id=\"circ10\" was not injected: check your FXML file 'view.fxml'.";
+        assert box11 != null : "fx:id=\"box11\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ11 != null : "fx:id=\"circ11\" was not injected: check your FXML file 'view.fxml'.";
+        assert box12 != null : "fx:id=\"box12\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ12 != null : "fx:id=\"circ12\" was not injected: check your FXML file 'view.fxml'.";
+        assert box13 != null : "fx:id=\"box13\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ13 != null : "fx:id=\"circ13\" was not injected: check your FXML file 'view.fxml'.";
+        assert box14 != null : "fx:id=\"box14\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ14 != null : "fx:id=\"circ14\" was not injected: check your FXML file 'view.fxml'.";
+        assert box15 != null : "fx:id=\"box15\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ15 != null : "fx:id=\"circ15\" was not injected: check your FXML file 'view.fxml'.";
+        assert box16 != null : "fx:id=\"box16\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ16 != null : "fx:id=\"circ16\" was not injected: check your FXML file 'view.fxml'.";
+        assert box17 != null : "fx:id=\"box17\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ17 != null : "fx:id=\"circ17\" was not injected: check your FXML file 'view.fxml'.";
+        assert box18 != null : "fx:id=\"box18\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ18 != null : "fx:id=\"circ18\" was not injected: check your FXML file 'view.fxml'.";
+        assert box19 != null : "fx:id=\"box19\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ19 != null : "fx:id=\"circ19\" was not injected: check your FXML file 'view.fxml'.";
+        assert box20 != null : "fx:id=\"box20\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ20 != null : "fx:id=\"circ20\" was not injected: check your FXML file 'view.fxml'.";
+        assert box21 != null : "fx:id=\"box21\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ21 != null : "fx:id=\"circ21\" was not injected: check your FXML file 'view.fxml'.";
+        assert box22 != null : "fx:id=\"box22\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ22 != null : "fx:id=\"circ22\" was not injected: check your FXML file 'view.fxml'.";
+        assert box23 != null : "fx:id=\"box23\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ23 != null : "fx:id=\"circ23\" was not injected: check your FXML file 'view.fxml'.";
+        assert box24 != null : "fx:id=\"box24\" was not injected: check your FXML file 'view.fxml'.";
+        assert circ24 != null : "fx:id=\"circ24\" was not injected: check your FXML file 'view.fxml'.";
+
+    }
+
+    
+}
